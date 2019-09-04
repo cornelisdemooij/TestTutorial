b94e19d7b96c3d9e91396f864a632fa49951ac49
Cornelis de Mooij <codemo@gmail.com>
2019-09-05 01:52:28 +0200

    First commit

---
+++ src/main/java/com/cornelisdemooij/testtutorial/Controller.java
@@ -0,0 +1,13 @@
+package com.cornelisdemooij.testtutorial;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
---
+++ src/main/java/com/cornelisdemooij/testtutorial/TesttutorialApplication.java
@@ -0,0 +1,23 @@
+package com.cornelisdemooij.testtutorial;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TesttutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesttutorialApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Hello TEP14! Ready for some testing?");
+		};
+	}
+
+}
