7cade9c1906fc21ae5a06a95b2ec034963652316
Cornelis de Mooij <codemo@gmail.com>
2019-09-15 22:11:11 +0200

    Added verification to the endpoint test

--- src/test/java/com/cornelisdemooij/testtutorial/api/QuestionEndpointTests.java
+++ src/test/java/com/cornelisdemooij/testtutorial/api/QuestionEndpointTests.java
@@ -1,7 +1,9 @@
 package com.cornelisdemooij.testtutorial.api;
 
 import com.cornelisdemooij.testtutorial.domain.Question;
+import com.cornelisdemooij.testtutorial.persistence.QuestionRepository;
 import com.cornelisdemooij.testtutorial.service.QuestionService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -27,16 +29,39 @@ public class QuestionEndpointTests {
     @MockBean
     private QuestionService questionService;
 
-    @Test
-    public void getQuestionsFromEndpointWithMockedService() throws Exception {
-        Question[] questionArray = { new Question("Does this answer your question?") };
+    @MockBean
+    private QuestionRepository questionRepo;
+
+    @Before
+    public void setUp() {
+        Question[] questionArray = {
+            new Question("Does this answer your question?"),
+            new Question("How about this?")
+        };
         Iterable<Question> questions = Arrays.asList(questionArray);
 
+        when(questionService.findById(questionArray[0].getId())).thenReturn(questionArray[0]);
+        when(questionService.findById(questionArray[1].getId())).thenReturn(questionArray[1]);
         when(questionService.findAll()).thenReturn(questions);
+    }
 
+    @Test
+    public void getQuestionsFromEndpointWithMockedService() throws Exception {
         ResponseEntity<String> entity = this.restTemplate.getForEntity("/api/question", String.class);
+        String expectedResponse = "[{\"id\":0,\"text\":\"Does this answer your question?\",\"length\":31},{\"id\":0,\"text\":\"How about this?\",\"length\":15}]";
+
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(entity.getBody()).isEqualTo("[{\"id\":0,\"text\":\"Does this answer your question?\",\"length\":31}]");
+        assertThat(entity.getBody()).isEqualTo(expectedResponse);
+
+        verify(questionService, never()).findById((long)0);
+        verify(questionService, never()).findById((long)1);
+
+        verify(questionService, never()).findById(anyLong());
+
+        verify(questionService).findAll();
+        verifyNoMoreInteractions(questionService);
+
+        verifyZeroInteractions(questionRepo);
     }
 
 }
